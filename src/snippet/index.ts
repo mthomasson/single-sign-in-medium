c39442cf48b276e99263f839e6bdf282b2c3f295
Max <maxime@interact.io>
2020-08-13 16:52:25 +0200

    Added register different methods on the window object

--- src/snippet/index.ts
+++ src/snippet/index.ts
@@ -1,4 +1,5 @@
 import {IframeUtils} from "./IframeUtils";
+import {MethodsService} from "./methodsService";
 
 const tracker = document.currentScript as HTMLScriptElement;
 
@@ -6,4 +7,6 @@ const tracker = document.currentScript as HTMLScriptElement;
 (function main() {
     const path = tracker.src.replace('/snippet.js', '');
     IframeUtils.createIframe(path);
+    const methodService= new MethodsService();
+    methodService.registerWindowMethods(path)
 })();
---
+++ src/snippet/methodsService.ts
@@ -0,0 +1,34 @@
+import {IframeEventsEnum} from "../shared/enums/iframe-events.enum";
+import {ScriptEventsEnum} from "../shared/enums/script-events.enum";
+import {IframeUtils} from "./IframeUtils";
+import {MessageEventModel} from "../shared/models/MessageEvent.model";
+
+export class MethodsService {
+    constructor() {
+    }
+
+    public registerWindowMethods(path: string) {
+        if (window) {
+            window.io3SingleSignIn = {
+                getAuthToken: this.getAuthToken,
+                storeAuthToken: this.storeAuthToken,
+                deleteAuthToken: this.deleteAuthToken,
+                origin: path,
+            }
+        } else {
+            throw new Error('You need to be in a browser!')
+        }
+    }
+
+    public getAuthToken(): Promise<string> {
+        return new Promise(()=>{});
+    }
+
+    public storeAuthToken(authToken: string) {
+        return new Promise(()=>{});
+    }
+
+    public deleteAuthToken(authToken: string) {
+        return new Promise(()=>{});
+    }
+}
